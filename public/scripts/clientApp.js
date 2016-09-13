f08a9ec04d848a2f97eda536053386e869dc7056
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-09-12 20:54:16 -0500

    adds in all scoket code

--- public/scripts/clientApp.js
+++ public/scripts/clientApp.js
@@ -1,3 +1,6 @@
+var socket = io();
+
+
 var IOT = angular.module('IOT', []);
 
 
--- server/app.js
+++ server/app.js
@@ -1,21 +1,32 @@
-var path = require('path');
 var express = require('express');
+var app = express();
+var socketio = require('socket.io');
+var path = require('path');
 var router = express.Router();
-
 var bodyParser = require('body-parser');
 
-var app = express();
 
 app.use(bodyParser.json());
 
 //Static Page
 app.use(express.static('public'));
 
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+console.log('a user is connected');
+
+socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+
 // Route inclusion
 var base = require('./routes/base');
 
 app.use('/', base);
 
-app.listen(process.env.PORT || 3000, function(){
+var server = app.listen(process.env.PORT || 3000, function(){
   console.log('listening on server 3000');
 });
