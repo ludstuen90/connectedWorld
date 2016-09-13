9062fff17a163ffc2dc479ce6b28a6bcc6a58682
Lukas Udstuen <Lukas.Udstuen@gmail.com>
2016-09-12 20:14:38 -0500

    weather page initialized

--- public/scripts/clientApp.js
+++ public/scripts/clientApp.js
@@ -3,5 +3,33 @@ var IOT = angular.module('IOT', []);
 
 IOT.controller('baseController', ['$scope', '$http', function($scope, $http){
   console.log('hello, world!');
-  
+
+
+
+$http({
+  method: 'GET',
+  url: 'http://api.wunderground.com/api/ff82541ce72353e5/conditions/q/MN/Minneapolis.json'
+}).then(function(response){
+  console.log(response.data.current_observation.feelslike_f);
+  $scope.currentObservations = response.data.current_observation;
+  $scope.feelsLikeF = $scope.currentObservations.feelslike_f;
+  $scope.weather = $scope.currentObservations.weather;
+  $scope.lastUpdated = $scope.currentObservations.observation_time;
+  console.log($scope.currentObservations);
+}).then(function(){
+
+  $http({
+    method: 'GET',
+    url: 'http://api.wunderground.com/api/ff82541ce72353e5/forecast10day/q/MN/Minneapolis.json'
+  }).then(function(response){
+    $scope.forecastArray = response.data.forecast.simpleforecast.forecastday;
+
+    console.log('forecast data', response.data);
+    console.log($scope.forecastArray);
+  });
+
+  });
+
+
+
 }]);
